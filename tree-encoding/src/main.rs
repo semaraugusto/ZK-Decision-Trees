f92c66e7855185d604f15ddfb0f1c564ef201ed5
semaraugusto <semaraugusto@dcc.ufmg.br>
2022-05-05 15:44:12 -0300

    add serde_json serialize method

--- tree-encoding/src/main.rs
+++ tree-encoding/src/main.rs
@@ -1,6 +1,24 @@
+// pub struct BinaryTree<T> {
+//     head: Option<Node<T>>,
+// }
+// impl<T> BinaryTree<T> {
+//     fn new(head: Node<T>) -> BinaryTree<T> {
+//         BinaryTree { head: Some(head) }
+//     }
+// }
+
+extern crate serde;
+extern crate serde_json;
+
+// Import this crate to derive the Serialize and Deserialize traits.
+#[macro_use]
+extern crate serde_derive;
+
+use std::fs::File;
+
 type ChildNode<T> = Option<Box<Node<T>>>;
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Node<T> {
     /// The actual data which will be stored within the tree
     pub data: T,
@@ -8,13 +26,24 @@ pub struct Node<T> {
     right_child: ChildNode<T>,
 }
 
-pub struct BinaryTree<T> {
-    head: Option<Node<T>>,
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EncodedTree<T> {
+    data_vec: Vec<T>,
+    is_node_vec: Vec<bool>,
+}
+
+impl<T> EncodedTree<T> {
+    fn new(data_vec: Vec<T>, is_node_vec: Vec<bool>) -> EncodedTree<T> {
+        EncodedTree {
+            data_vec,
+            is_node_vec,
+        }
+    }
 }
 
 impl<T> Node<T>
 where
-    T: std::fmt::Debug + Copy,
+    T: std::fmt::Debug + Copy + serde::Serialize,
 {
     fn new(data: T) -> Node<T> {
         Node {
@@ -23,16 +52,16 @@ where
             data,
         }
     }
-    fn add_right_child(&mut self, data: T) {
+    pub fn add_right_child(&mut self, data: T) {
         let child = Node::new(data);
         self.right_child = Some(Box::new(child));
     }
-    fn add_left_child(&mut self, data: T) {
+    pub fn add_left_child(&mut self, data: T) {
         let child = Node::new(data);
         self.left_child = Some(Box::new(child));
     }
 
-    fn print_tree(&self) {
+    pub fn print_tree(&self) {
         println!("Data: {:?}", self.data);
         if let Some(child) = &self.left_child {
             print!("Left: ");
@@ -43,12 +72,15 @@ where
             child.print_tree();
         }
     }
-    fn encode_tree(&self) -> (Vec<T>, Vec<bool>) {
+    pub fn encode_tree(&self) -> EncodedTree<T> {
         let mut data_vec: Vec<T> = vec![self.data];
         let mut is_node_vec: Vec<bool> = vec![true];
         self._encode_tree(&mut data_vec, &mut is_node_vec);
 
-        (data_vec, is_node_vec)
+        EncodedTree {
+            data_vec,
+            is_node_vec,
+        }
     }
 
     fn _encode_tree(&self, data_vec: &mut Vec<T>, is_node_vec: &mut Vec<bool>) {
@@ -73,16 +105,27 @@ where
             }
         }
     }
-}
 
-impl<T> BinaryTree<T> {
-    fn new(head: Node<T>) -> BinaryTree<T> {
-        BinaryTree { head: Some(head) }
+    fn to_json(&self) -> serde_json::Result<String> {
+        let encoded_tree = self.encode_tree();
+
+        serde_json::to_string(&encoded_tree)
+    }
+
+    fn save_tree(&self, filename: &str, pretty: bool) -> serde_json::Result<()> {
+        let encoded_tree = self.encode_tree();
+        // let json = self.to_json()?;
+
+        if pretty {
+            serde_json::to_writer_pretty(&File::create(filename).unwrap(), &encoded_tree)
+        } else {
+            serde_json::to_writer(&File::create(filename).unwrap(), &encoded_tree)
+        }
     }
 }
 
 fn main() {
-    let mut root = Node::new(10);
+    let mut root: Node<i32> = Node::new(10);
     root.add_left_child(20);
     root.add_right_child(30);
 
@@ -90,7 +133,8 @@ fn main() {
     root.left_child.as_mut().unwrap().add_right_child(50);
     root.right_child.as_mut().unwrap().add_right_child(70);
     // root.print_tree();
-    let (data_vec, is_node_vec) = root.encode_tree();
-    println!("Data: {:?}", data_vec);
-    println!("node_vec: {:?}", is_node_vec);
+    let encoded = root.to_json();
+    println!("Data: {:?}", encoded);
+    let res = root.save_tree("tree.json", true);
+    println!("res: {:?}", res);
 }
